75bf64c0f13bdf0d1643a9ace50f8a097ea7c49e
Takesure Mamvota <tooyoungmamvota@gmail.com>
2023-11-16 02:23:31 +0700

    responsive

--- src/features/portfolio/Portfolio.tsx
+++ src/features/portfolio/Portfolio.tsx
@@ -1,6 +1,17 @@
 import pizza from '../../assets/pizza.jpg'
 import movie from '../../assets/movie.png'
 import worldwise from '../../assets/worldwise.jpg'
+import {
+    BiBookContent,
+    BiGitMerge,
+    BiLink,
+    BiLogoGithub,
+    BiLogoJavascript,
+    BiLogoRedux,
+    BiLogoTypescript
+} from "react-icons/bi";
+import {DiReact} from "react-icons/di";
+import {SiCssmodules, SiReactrouter, SiTailwindcss} from "react-icons/si";
 
 export const Portfolio = () => {
     return (
@@ -9,42 +20,80 @@ export const Portfolio = () => {
                 <div className='relative flex flex-col cursor-pointer overflow-hidden'>
                     <figure className='relative hover:scale-105 hover:opacity-80 w-full rounded-lg overflow-hidden mb-8 transition-all duration-500'>
                         <img className='object-cover w-full h-full rounded-lg transition-all duration-700' src={movie} alt="tech" />
-                        {/*<div className='project-item-icon-box'>*/}
-                        {/*    <AiOutlineEye className='text-2xl'/>*/}
-                        {/*</div>*/}
 
                     </figure>
-                    <div className='flex flex-col '>
-                        <p className='text-lg font-bold text-stone-100'>Entertainment</p>
-                        <p className='text-sm text-stone-500'>Web Development</p>
+                    <div className='flex flex-col'>
+                        <div className='flex gap-2 mb-3 text-zinc-500 text-lg md:text-xl'>
+                            <DiReact/>
+                            <SiTailwindcss/>
+                            <SiReactrouter/>
+                            <BiLogoTypescript/>
+                            <BiGitMerge/>
+                        </div>
+                        <div className='flex justify-between'>
+                            <div className='flex flex-col '>
+                                <p className='text-lg font-bold text-stone-100'>Entertainment</p>
+                                <p className='text-sm text-stone-500'>Web Development</p>
+                            </div>
+                            <div className='flex gap-2 md:gap-6'>
+                                <BiLogoGithub className='md:text-xl text-lg hover:text-amber-200 transition-all duration-500'/>
+                                <BiLink className='md:text-xl text-lg hover:text-amber-200 transition-all duration-500'/>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
                 <div className='relative flex flex-col cursor-pointer overflow-hidden'>
                     <figure className='relative hover:scale-105 hover:opacity-80 w-full h-full rounded-lg overflow-hidden mb-10 before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-transparent before:z-10 transition-all duration-500'>
                         <img className='object-cover rounded-lg transition-all duration-700' src={pizza} alt="tech" />
-                        {/*<div className='project-item-icon-box'>*/}
-                        {/*    <AiOutlineEye className='text-2xl'/>*/}
-                        {/*</div>*/}
 
                     </figure>
-                    <div className='flex flex-col '>
-                        <p className='text-lg font-bold text-stone-100'>Food</p>
-                        <p className='text-sm text-stone-500'>Web Development</p>
+                    <div className='flex flex-col'>
+                        <div className='flex gap-2 mb-3 text-zinc-500 text-lg md:text-xl'>
+                            <DiReact/>
+                            <SiTailwindcss/>
+                            <SiReactrouter/>
+                            <BiLogoTypescript/>
+                            <BiGitMerge/>
+                            <BiLogoRedux/>
+                        </div>
+                        <div className='flex justify-between'>
+                            <div className='flex flex-col '>
+                                <p className='text-lg font-bold text-stone-100'>Food</p>
+                                <p className='text-sm text-stone-500'>Web Development</p>
+                            </div>
+                            <div className='flex gap-2 md:gap-6'>
+                                <BiLogoGithub className='md:text-xl text-lg hover:text-amber-200 transition-all duration-500'/>
+                                <BiLink className='md:text-xl text-lg hover:text-amber-200 transition-all duration-500'/>
+                            </div>
+                        </div>
                     </div>
+
                 </div>
                 <div className='relative flex flex-col cursor-pointer overflow-hidden'>
                     <figure className='relative hover:scale-105 hover:opacity-80 w-full h-full rounded-lg overflow-hidden mb-10 before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-transparent before:z-10 transition-all duration-500'>
                         <img className='object-cover rounded-lg transition-all duration-700' src={worldwise} alt="tech" />
-                        {/*<div className='project-item-icon-box'>*/}
-                        {/*    <AiOutlineEye className='text-2xl'/>*/}
-                        {/*</div>*/}
-
                     </figure>
-                    <div className='flex flex-col '>
-                        <p className='text-lg font-bold text-stone-100'>Travel</p>
-                        <p className='text-sm text-stone-500'>Web Development</p>
+                    <div className='flex flex-col'>
+                        <div className='flex gap-2 mb-3 text-zinc-500 text-lg md:text-xl'>
+                            <DiReact/>
+                            <SiCssmodules/>
+                            <BiBookContent/>
+                            <BiLogoJavascript/>
+                            <BiGitMerge/>
+                        </div>
+                        <div className='flex justify-between'>
+                            <div className='flex flex-col '>
+                                <p className='text-lg font-bold text-stone-100'>Travel</p>
+                                <p className='text-sm text-stone-500'>Web Development</p>
+                            </div>
+                            <div className='flex gap-2 md:gap-6'>
+                                <BiLogoGithub className='md:text-xl text-lg hover:text-amber-200 transition-all duration-500'/>
+                                <BiLink className='md:text-xl text-lg hover:text-amber-200 transition-all duration-500'/>
+                            </div>
+                        </div>
                     </div>
+
                 </div>
 
             </div>
